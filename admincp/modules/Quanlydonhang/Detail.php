c8d9b29519ba30b013cc039f529ff0f74b6571c1
lanpt2105 <64910129+lanpt2105@users.noreply.github.com>
2020-05-22 11:37:25 +0700

    Update Detail.php

--- admincp/modules/Quanlydonhang/Detail.php
+++ admincp/modules/Quanlydonhang/Detail.php
@@ -9,6 +9,7 @@
 ?>
 <div class="">
     <table width="80%" border="1" style="margin-left: 20px;margin-bottom:5px;float: left;" class="table-hover">
+        <caption>Thông tin chi tiết đơn hàng</caption>
         <thead>
             <tr>
                 <th>STT</th>
@@ -20,17 +21,25 @@
         </thead>
         <tbody>
             <?php
-                $i = 0;
+                $i = 1;
                 while($dong = mysqli_fetch_array($row_list)){
+                    $find = strpos($dong['HinhAnh'], 'http');
+                    if( $find !== false)
+                        $image = $dong['HinhAnh'];
+                    else
+                        $image = '../'.$dong['HinhAnh'];
             ?>
                 <tr>
-                    <td><?php echo $i ?></td>
+                    <td><?php echo $i; ?></td>
                     <td><?php echo $dong['MaSP']?></td>
                     <td><?php echo $dong['TenSP']?></td>
-                    <td><img src="../<?php echo $dong['HinhAnh']?>" alt="<?php echo $dong['TenSP']?>" width="100px"></td>
+                    <td>
+                        <img src="<?php echo $image ?>" alt="<?php echo $dong['TenSP']?>" width="100p">
+                    </td>
                     <td><?php echo $dong['DonGia']?></td>
                 </tr>
             <?php
+              $i++;
                 }
             ?>
         </tbody>
